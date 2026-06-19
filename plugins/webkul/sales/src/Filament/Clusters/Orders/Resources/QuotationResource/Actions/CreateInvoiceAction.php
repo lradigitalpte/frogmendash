f206296d3f0a445a0efd9d20456ea9944f67aefb
henry-codex <henryteimensah@gmail.com>
2026-06-19 11:48:38 +0000

    fixed

--- plugins/webkul/sales/src/Filament/Clusters/Orders/Resources/QuotationResource/Actions/CreateInvoiceAction.php
+++ plugins/webkul/sales/src/Filament/Clusters/Orders/Resources/QuotationResource/Actions/CreateInvoiceAction.php
@@ -64,7 +64,11 @@ class CreateInvoiceAction extends Action
             ])
             ->hidden(fn ($record) => $record->invoice_status != InvoiceStatus::TO_INVOICE)
             ->action(function (Order $record, $data) {
-                if ($record->qty_to_invoice == 0) {
+                $hasInvoiceableLine = $record->lines->contains(
+                    fn ($line) => $line->invoice_status == InvoiceStatus::TO_INVOICE
+                );
+
+                if (! $hasInvoiceableLine) {
                     Notification::make()
                         ->title(__('sales::filament/clusters/orders/resources/quotation/actions/create-invoice.notification.no-invoiceable-lines.title'))
                         ->body(__('sales::filament/clusters/orders/resources/quotation/actions/create-invoice.notification.no-invoiceable-lines.body'))
